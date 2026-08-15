1eafc41eda048d83a094e7d463c92e8dd0194fd2
Russell <lqdfx@hotmail.com>
2026-08-15 15:35:58 -0400

    Fix Ghast Griefing not being canceled

--- src/main/java/work/lqdfxnet/lqdfxsextras/EntityRules/mobGriefing.java
+++ src/main/java/work/lqdfxnet/lqdfxsextras/EntityRules/mobGriefing.java
@@ -3,9 +3,9 @@ package work.lqdfxnet.lqdfxsextras.EntityRules;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.monster.EnderMan;
-import net.minecraft.world.entity.monster.Ghast;
 import net.minecraft.world.entity.monster.Silverfish;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.hurtingprojectile.Fireball;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityMobGriefingEvent;
@@ -16,7 +16,7 @@ import work.lqdfxnet.lqdfxsextras.ModConfigCommon;
 public class mobGriefing {
 
     @SubscribeEvent
-    public static void griefingFarmland(BlockEvent.FarmlandTrampleEvent event) {
+    public static void trampleFarmland(BlockEvent.FarmlandTrampleEvent event) {
 
         // Server side event only
         if (event.getEntity().level().isClientSide()) return;
@@ -27,7 +27,7 @@ public class mobGriefing {
     }
 
     @SubscribeEvent
-    public static void griefingMobEntity(final EntityMobGriefingEvent event) {
+    public static void MobEntityGriefing(final EntityMobGriefingEvent event) {
 
         // Server side event only
         if (event.getEntity().level().isClientSide()) return;
@@ -35,11 +35,11 @@ public class mobGriefing {
         Entity entity = event.getEntity();
 
         if (entity instanceof Player) { return; }
-        if (entity instanceof EnderMan && !endermanGriefingEnabled()) { event.setCanGrief(false); return; }
-        if (entity instanceof Creeper && !creeperGriefingEnabled()) { event.setCanGrief(false); return; }
-        if (entity instanceof Silverfish && !silverfishGriefingEnabled()) { event.setCanGrief(false); return; }
-        if (entity instanceof Ghast && !ghastGriefingEnabled()) { event.setCanGrief(false); }
-        else { event.setCanGrief(true); }
+        if ((entity instanceof EnderMan) && !endermanGriefingEnabled()) { event.setCanGrief(false); return; }
+        if ((entity instanceof Creeper) && !creeperGriefingEnabled()) { event.setCanGrief(false); return; }
+        if ((entity instanceof Silverfish) && !silverfishGriefingEnabled()) { event.setCanGrief(false); return; }
+        if ((entity instanceof Fireball) && !ghastGriefingEnabled()) { event.setCanGrief(false); }
+        event.setCanGrief(true);
 
     }
 
